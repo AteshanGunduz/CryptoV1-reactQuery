b5f6ff08d01eeb30465cce9031f624db5cdb13a4
AteshanGunduz <atshn.gunduz@gmail.com>
2024-02-19 11:11:10 -1100

    update

--- src/components/Profiles.tsx
+++ src/components/Profiles.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { clearFormData, setProfile } from "../features/profileSlice";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Profiles = () => {
 
